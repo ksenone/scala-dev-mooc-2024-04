04f5081bab371c2d2563a69c84504a85e5095818
Ksenia Khomutova <kseniahomutova4@gmail.com>
2024-08-08 21:17:37 +0700

    Выполнила ДЗ №4: реализовала метод для списка Future

--- src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
+++ src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
@@ -3,7 +3,9 @@ package ru.otus.module1.futures
 
 import ru.otus.module1.futures.HomeworksUtils.TaskSyntax
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+import scala.annotation.tailrec
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 object task_futures_sequence {
 
@@ -21,7 +23,32 @@ object task_futures_sequence {
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`" ()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    case class FuturesResults(success: List[A], failures: List[Throwable])
+
+    @tailrec
+    def completeFuture(future: Future[A], acc: FuturesResults): FuturesResults = if (future.isCompleted) {
+      future.value match {
+        case Some(Success(value)) => FuturesResults(acc.success :+ value, acc.failures)
+        case Some(Failure(exception)) => FuturesResults(acc.success, acc.failures :+ exception)
+      }
+    } else completeFuture(future, acc)
+
+    @tailrec
+    def go(
+      futuresList: List[Future[A]],
+      acc: FuturesResults
+    ): Promise[(List[A], List[Throwable])] = futuresList match {
+      case Nil =>
+        val promise = Promise[(List[A], List[Throwable])]
+        promise.success(acc.success, acc.failures)
+
+      case head :: tail =>
+        go(tail, completeFuture(head, acc))
+    }
+
+    go(futures, FuturesResults(Nil, Nil)).future
+  }
 
 }
